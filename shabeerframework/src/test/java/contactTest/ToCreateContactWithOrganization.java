a13ec7ca3a0aeb1047d1c6ffe7f147b2e3431a2d
91778 <91778@MSI>
2024-05-13 17:42:09 +0530

    Added new class

---
+++ shabeerframework/src/test/java/contactTest/ToCreateContactWithOrganization.java
@@ -0,0 +1,10 @@
+package contactTest;
+
+public class ToCreateContactWithOrganization {
+
+	public static void main(String[] args) {
+
+		System.out.println("Added contact with organization");
+	}
+
+}
