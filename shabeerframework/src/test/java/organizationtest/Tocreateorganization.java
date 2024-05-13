d2dfdbc72bfda3b28b4a0633fe1bf143329c658e
Jayaram <Jayaram@jayaram>
2024-05-13 17:33:46 +0530

    shabeerframework

---
+++ shabeerframework/src/test/java/organizationtest/Tocreateorganization.java
@@ -0,0 +1,12 @@
+package organizationtest;
+
+public class Tocreateorganization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("created successfully");
+
+	}
+
+}
